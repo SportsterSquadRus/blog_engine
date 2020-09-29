4e92c30f9d9e8a45ab3eef955bbd2eb21046e847
suren <surovblog@gmail.com>
2020-09-29 21:49:42 +0300

    urls edited

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', PostsListView.as_view(), name='posts_list_url'),
     path('post/detail/<int:pk>/', PostDetailView.as_view(), name='post_detail_url'),
     path('post/create/', PostCreateView.as_view(), name='post_create_url'),
-    path('post/detail/<int:pk>/', PostDetailView.as_view(), name='post_detail_url'),
     path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete_url'),
     path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update_url'),
     path('author/<str:username>/', AuthorPostsView.as_view(), name='author_posts_list_url'),
