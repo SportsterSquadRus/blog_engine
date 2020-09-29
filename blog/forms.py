9f873209c8ba3d7fc0fcb9d6a21218ca2c9ab8bb
suren <surovblog@gmail.com>
2020-09-29 20:27:26 +0300

    inteli truncate

--- blog/forms.py
+++ blog/forms.py
@@ -5,10 +5,11 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'body', 'draft_status')
+        fields = ('title', 'body', 'cover_url', 'draft_status',)
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'cover_url':forms.URLInput(attrs={'class': 'form-control'}),
             'draft_status': forms.CheckboxInput(attrs={'class': 'form-check'})
         }
--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,9 @@ class Post(models.Model):
     body = models.TextField(verbose_name='Текст')
     date_pub = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey('auth.User', blank=True, null=True, on_delete=models.CASCADE)
-    cover_url = models.URLField(verbose_name='Ссылка на обложка', max_length=300, blank=True, null=True)
+    cover_url = models.URLField(verbose_name='Ссылка на обложку', max_length=300, blank=True, null=True)
     draft_status = models.BooleanField(verbose_name='Черновик', default=False)
+    truncate = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'pk': self.pk})
