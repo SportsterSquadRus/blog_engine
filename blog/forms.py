530b4f033c135d13419d31ea3e591b6d3d588cdf
SportsterSquadRus <59732122+SportsterSquadRus@users.noreply.github.com>
2020-10-08 11:41:59 +0300

    banned words filter ready

--- blog/forms.py
+++ blog/forms.py
@@ -4,6 +4,12 @@ from taggit.forms import TagWidget
 
 
 class PostForm(forms.ModelForm):
+    STOP_LIST = [
+                'блядь',
+                'бляди',
+                'пизда',
+                'пиздец',
+            ]
     class Meta:
         model = Post
         fields = ('title', 'body', 'cover_url', 'draft_status', 'tags')
@@ -15,3 +21,24 @@ class PostForm(forms.ModelForm):
             'draft_status': forms.CheckboxInput(attrs={'class': 'form-check'}),
             'tags': TagWidget(attrs={'class': 'form-control'}),
         }
+
+    def clean_body(self):        
+        text = self.cleaned_data['body']
+        for word in self.STOP_LIST:
+            if word in text:
+                raise forms.ValidationError("Вы позволили себе немного лишнего! Исправьте текст!")
+        return text
+    
+    def clean_title(self):        
+        text = self.cleaned_data['title']
+        for word in self.STOP_LIST:
+            if word in text:
+                raise forms.ValidationError("Вы позволили себе немного лишнего! Исправьте текст!")
+        return text
+
+    def clean_tags(self):        
+        text = self.cleaned_data['tags']
+        for word in self.STOP_LIST:
+            if word in text:
+                raise forms.ValidationError("Вы позволили себе немного лишнего! Исправьте текст!")
+        return text
