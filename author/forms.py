741a9a5b80a5cfb35758018430399c5ecfcf713b
suren <a3a3a@list.ru>
2020-10-11 15:26:42 +0300

    hide_email field added

--- author/forms.py
+++ author/forms.py
@@ -7,10 +7,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
 
         model = Profile
-        fields = ('date_birth',)
+        fields = ('date_birth', 'email_hidden')
 
         widgets = {
-            'date_birth': forms.DateInput(attrs={'class': 'form-control'})
+            'date_birth': forms.DateInput(attrs={'class': 'form-control'}),
+            'email_hidden': forms.CheckboxInput(attrs={'class': 'form-check'})
+
         }
 
 
