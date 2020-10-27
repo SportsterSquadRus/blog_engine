285340ea1ed1270294436f9f4ab1ffa2088b9e51
SportsterSquadRus <59732122+SportsterSquadRus@users.noreply.github.com>
2020-10-27 12:43:16 +0300

    object complaint func init

---
+++ complaint/utils.py
@@ -0,0 +1,6 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import models
+
+def ObjectComplaintFunc(request, pk, model):
+    pass
--- complaint/views.py
+++ complaint/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from .utils import ObjectComplaintFunc
 
+@login_required
 def PostComplaintView(request, pk):
-    pass
+    print('lala')
+    return redirect(reverse('post_detail_url', args=[str(pk)]))
 
+@login_required
 def CommentComplaintView(request, pk):
     pass
