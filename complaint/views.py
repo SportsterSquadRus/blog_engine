931f47b5cbb56fe24df666a82921e347a144695f
SportsterSquadRus <59732122+SportsterSquadRus@users.noreply.github.com>
2020-10-27 13:40:38 +0300

    utils.py deleted

--- complaint/views.py
+++ complaint/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .utils import ObjectComplaintFunc
 from blog.models import Post
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import models
+from django.core.mail import send_mail
+from blog_engine import passwords
 
 @login_required
 def PostComplaintView(request, pk):
-    ObjectComplaintFunc(request, pk, Post)
+    obj = get_object_or_404(Post, id=pk)
+    user = models.User.objects.get(pk=request.user.id)
+    send_mail(
+    'Жалоба на пост от пользователя {}.'.format(user),
+    'http://127.0.0.1:8000{}'.format(obj.get_absolute_url()),
+    passwords.email_pass()[0],
+    [passwords.stuff()],
+    fail_silently=False,
+)
     return redirect(reverse('post_detail_url', args=[str(pk)]))
 
 @login_required
