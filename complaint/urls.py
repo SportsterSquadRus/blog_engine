a477d792d8c22ddee9093f6b3b649e82de3fa2f9
Suren <surovblog@gmail.com>
2020-10-27 20:32:29 +0300

    css added, comments_complaint view inited, template changed

--- complaint/urls.py
+++ complaint/urls.py
@@ -4,5 +4,4 @@ from .views import PostComplaintView, CommentComplaintView
 urlpatterns = [
     path('post/<int:pk>/', PostComplaintView, name='complaint_post'),
     path('comment/<int:pk>/', CommentComplaintView, name='complaint_comment'),
-
 ]
