b7664986248b50a0c26223e2a4ecacc6018edd8f
SportsterSquadRus <59732122+SportsterSquadRus@users.noreply.github.com>
2020-10-27 12:53:18 +0300

    function call

--- complaint/utils.py
+++ complaint/utils.py
@@ -3,4 +3,5 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import models
 
 def ObjectComplaintFunc(request, pk, model):
-    pass
+    obj = get_object_or_404(model, id=pk)
+    user = models.User.objects.get(pk=request.user.id)
--- complaint/views.py
+++ complaint/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from .utils import ObjectComplaintFunc
+from blog.models import Post
 
 @login_required
 def PostComplaintView(request, pk):
-    print('lala')
+    ObjectComplaintFunc(request, pk, Post)
     return redirect(reverse('post_detail_url', args=[str(pk)]))
 
 @login_required
