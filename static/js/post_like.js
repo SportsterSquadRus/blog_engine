1feda3d1b9f953ff8be3f70a7658dae1629cca52
suren <elek34ka@gmail.com>
2020-11-02 00:28:13 +0300

    js separate file

---
+++ static/js/post_like.js
@@ -0,0 +1,43 @@
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
+
+$(document).ready(function(){
+  $('.likin').click(function(){
+    $.ajax({
+              type: "POST",
+              url: $(this).attr('id'),
+              data: {'content_id': $(this).attr('name'),'operation':'like_submit','csrfmiddlewaretoken': getCookie('csrftoken')},
+              dataType: "json",
+              success: function(response) {
+              selector = document.getElementsByName(response.content_id);
+                    if(response.liked==true){
+                      $(selector).css("color","blue");
+                      $(selector).attr('title', 'Всего лайков: ' + response.likes_count);
+                    }
+                    else if(response.liked==false){
+                      $(selector).css("color","black");
+                      $(selector).attr('title', 'Всего лайков: ' + response.likes_count);
+                    }
+  
+  
+              }
+  
+        });
+  
+  });
+
+});
