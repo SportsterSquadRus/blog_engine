6e154580ddd6c82f92cb9834e925cb441de46c2a
suren <a3a3a@list.ru>
2020-12-02 14:35:26 +0300

    redirect

--- blog_engine/urls.py
+++ blog_engine/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import redirect_view
 
 urlpatterns = [
+    path('', redirect_view),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
